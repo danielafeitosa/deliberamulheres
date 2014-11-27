08eceac7f704a86334c3a68e4bb8e5fed9e8b3d1
Daniela Feitosa <danielafeitosa@colivre.coop.br>
2014-11-26 23:53:40 -0300

    Replaced method name

--- app/models/deputado.rb
+++ app/models/deputado.rb
@@ -3,5 +3,9 @@ class Deputado < ActiveRecord::Base
   mount_uploader :foto, PictureUploader
 
   belongs_to :partido_id
-  attr_accessible :anexo, :data_falecimento, :data_nascimento, :email, :fone, :foto, :gabinete, :id_parlamentar, :id_parlamentar_deprecated, :ide_cadastro, :matricula, :nome, :nome_civil, :nome_parlamentar, :nome_parlamentar_atual, :nome_profissao, :num_legislatura, :sexo, :status, :status_legislatura_atual, :uf, :uf_representacao_atual, :url_foto
+  attr_accessible :anexo, :data_falecimento, :data_nascimento, :email, :fone,
+:foto, :gabinete, :id_parlamentar, :id_parlamentar_deprecated, :ide_cadastro,
+:matricula, :nome, :nome_civil, :nome_parlamentar, :nome_parlamentar_atual,
+:nome_profissao, :num_legislatura, :sexo, :status,
+:situacao_legislatura_atual, :uf, :uf_representacao_atual, :url_foto
 end
