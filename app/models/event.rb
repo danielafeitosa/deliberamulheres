dfc1b307405ec90fc6ac675c7ca761c7c9fb1292
Daniela Feitosa <danielafeitosa@colivre.coop.br>
2014-11-27 10:10:18 -0300

    Enhancing edition of events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,7 @@
+# coding: utf-8
 class Event < ActiveRecord::Base
   attr_accessible :category, :description, :title
+
+  CATEGORIES = ['Coligação', 'Manifestação Popular', 'Mídia', 'Pizza', 'Reunião Extraordinária','Reunião Solene']
+
 end
