bd419c2403eea62017a5e44f11d92581eac2d665
Daniela Feitosa <danielafeitosa@colivre.coop.br>
2014-11-27 00:16:50 -0300

    Fixed relation name on deputado

--- app/models/deputado.rb
+++ app/models/deputado.rb
@@ -2,10 +2,10 @@ class Deputado < ActiveRecord::Base
 
   mount_uploader :foto, PictureUploader
 
-  belongs_to :partido_id
+  belongs_to :partido
   attr_accessible :anexo, :data_falecimento, :data_nascimento, :email, :fone,
 :foto, :gabinete, :id_parlamentar, :id_parlamentar_deprecated, :ide_cadastro,
 :matricula, :nome, :nome_civil, :nome_parlamentar, :nome_parlamentar_atual,
 :nome_profissao, :num_legislatura, :sexo, :status,
-:situacao_legislatura_atual, :uf, :uf_representacao_atual, :url_foto
+:situacao_legislatura_atual, :uf, :uf_representacao_atual, :url_foto, :partido
 end
